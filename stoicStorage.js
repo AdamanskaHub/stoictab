c5859c89d5da9824c2346a7742ae99c2609349e2
Emilie G <adamanska@gmail.com>
2018-05-21 16:29:14 +0200

    Changes on suffering box

--- stoicStorage.js
+++ stoicStorage.js
@@ -1,5 +1,28 @@
 // STORING STUFF !!!!!
 
 $(document).ready(function(){
+  
+  $('#sufferingBoxTwo').hide();
+
   const focusInput = document.getElementById('focusInput');
+
+  // const answerSuffering = (text) => {
+  //   const h3 = document.createElement('h3');
+  //   h3.textContent = text;
+  //   // document.getElementById("sufferingAnswer").appendChild(h3);
+  //   document.getElementById("suffering").remove();
+  // }
+
+  
+  document.getElementById("sufferingButton").addEventListener("click", function(){
+    var blaSuffering = $('#suffering').val();
+    console.log(blaSuffering);
+    $( "#sufferingBox" ).append( "<h3>Test</h3>" ).text(blaSuffering);
+    // answerSuffering(document.getElementById("suffering").val);
+    $("#suffering").remove();
+    $("#sufferingButton").remove();
+    $('#sufferingBoxTwo').show();
+  });
+
+
 });
